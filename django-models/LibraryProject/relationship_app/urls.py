f62f7f19074f6b77715e01b83e7433c7ee49071b
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-01-16 21:09:32 +0000

    let's see

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # Function-based view
@@ -9,7 +10,10 @@ urlpatterns = [
     # Class-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # Registration still uses custom view
     path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
 ]
+
