8503bace38ce9480b3ff3c599d839c493adfedd8
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-02-05 12:54:29 +0000

    Implementing Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/api/README.md
+++ advanced-api-project/api/README.md
@@ -15,3 +15,33 @@ Custom Features:
 
 - BookSerializer validates publication_year
 - BookListView supports optional ?author=Name filtering
+
+
+
+
+
+### Advanced Query Features
+
+The Book list endpoint supports filtering, searching, and ordering.
+
+#### Filtering
+- Filter by title:
+  /api/books/?title=Clean Code
+- Filter by publication year:
+  /api/books/?publication_year=2021
+- Filter by author ID:
+  /api/books/?author=1
+
+#### Search
+- Search by title or author name:
+  /api/books/?search=django
+
+#### Ordering
+- Order by title:
+  /api/books/?ordering=title
+- Order by publication year (descending):
+  /api/books/?ordering=-publication_year
+
+#### Combined Queries
+- Example:
+  /api/books/?search=python&ordering=-publication_year
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,26 +1,57 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
+from rest_framework import filters
 from .models import Book
 from .serializers import BookSerializer
-
+from  django_filters.rest_framework import DjangoFilterBackend
 
 # ListView - Retrieves all  books
 
 class BookListView(generics.ListAPIView):
     
+    """
+    Lists all books with support for:
+    - Filtering
+    - Searching
+    - Ordering
+
+    Filtering fields:
+        - title
+        - publication_year
+        - author (by ID)
+
+    Search fields:
+        - title
+        - author name
+
+    Ordering fields:
+        - title
+        - publication_year
+    """
+    
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [AllowAny]
     
-    def get_queryset(self):
-        """
-        Optionally filter by author name using query param ?author=Name
-        """
-        queryset = Book.objects.all()
-        author_name = self.request.query_params.get('author')
-        if author_name:
-            queryset = queryset.filter(author__name__icontains=author_name)
-        return queryset    
-        
+    filter_backends  = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    
+      # Filtering configuration
+    filterset_fields = ['title', 'publication_year', 'author']
+
+    # Search configuration
+    search_fields = ['title', 'author__name']
+
+    # Ordering configuration
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
+    
+    
+    
+      
     
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
