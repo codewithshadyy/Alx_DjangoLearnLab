1a035ccb722e77a6d34822c8e7fb3b8f4ac698e7
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-02-19 17:36:27 +0000

    Implementing Posts and Comments Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from .serializers import PostSerializer, CommentSerializer
 from .models import Post, Comment
 from .permissions import IsOwner
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.filters import SearchFilter
 
-class PostViewSet(ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by("-created_at")
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -16,7 +16,7 @@ class PostViewSet(ModelViewSet):
     def perform_create(self, serializer):
          serializer.save(author=self.request.user)
     
-class CommentViewSet(ModelViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated, IsOwner]
