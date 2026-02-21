69bea5eac5f5659d3f775207aa20bbea46abb3d9
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-02-21 07:40:38 +0000

    Implementing Notifications and Likes Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -50,7 +50,7 @@ class LikePostView(generics.GenericAPIView):
        def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
 
-        like, created = Like.objects.get_or_create(user=request.user,post=post)
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if not created:
             return Response(
@@ -76,7 +76,7 @@ class UnlikePostView(generics.GenericAPIView):
     def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
 
-        like = Like.objects.get_or_create(user=request.user,post=post)
+        like = Like.objects.filter(user=request.user,post=post)
 
         if not like.exists():
             return Response(
